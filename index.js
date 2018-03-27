6504274030ed17c67b58be02ef2bf742dc89fc6b
sebastian.bogado <seebogado@gmail.com>
2018-03-27 16:56:41 -0300

    Add passport

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
-app.get('/', (req, res) =>{
-  res.send({
-    name: 'Ernesto',
-    lastName; 'Albarez',
-    jokeName:'Misionero'
-  });
-});
+//New Google strategy creates a new google strategy instance
+passport.use(new GoogleStrategy());
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
